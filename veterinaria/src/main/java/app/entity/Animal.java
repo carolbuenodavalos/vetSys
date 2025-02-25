60e3b14e5ce18db439f3706b430a608dbf244dde
Caroline Bueno <carolinebuenodavalos@hotmail.com>
2025-02-25 20:43:47 -0300

    primeiro commit com a base do projeto

---
+++ veterinaria/src/main/java/app/entity/Animal.java
@@ -0,0 +1,17 @@
+package app.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Animal {
+	
+	private long id;
+	
+	private String especie;
+	private String raca;
+	private String idade;
+	
+
+}
